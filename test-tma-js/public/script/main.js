0a969ac8146dcb6ac7f29c68450c6ddb66502b18
nicolasplovie <nicolas.plovie@gmail.com>
2019-09-23 16:29:07 +0200

    delete useless files

--- test-tma-js/public/script/main.js
+++
@@ -1,9 +0,0 @@
-// $(document).on('ready', () => {
-auth.signIn('test', 'test')
-    .then(() => {
-        Socket.init(io)
-        users.init()
-        users.getAllUsers.subscribe((users)=> console.log(users))
-    })
-
-// })
